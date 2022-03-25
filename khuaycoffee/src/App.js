58e42ad56ca47eb6b0d01f2ba6daf4951c9be79a
viethuynh713 <66113968+viethuynh713@users.noreply.github.com>
2022-03-25 22:25:21 +0700

    Add Router

--- khuaycoffee/src/App.js
+++ khuaycoffee/src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
+import { Route, Routes, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <a>
-          Khuay Coffee
-        </a>
+      <Routes>
+        <Route path="/" element={<Banner />} />
+        <Route path="/admin/account" element={<Account />} />
+        <Route path="/admin/product" element={<Product />} />
+        <Route path="/admin/order" element={<Order/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product" element={<Category />} />
+        <Router path="/product/:id" element={<ProductDetail />} />
+
+      </Routes>
 
     </div>
   );
