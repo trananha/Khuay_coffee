fe2d104531e00d5e53d1a519321104bff3f1faea
PhongxTran <92942665+PhongxTran@users.noreply.github.com>
2022-03-27 22:32:52 +0700

    up demo

--- khuaycoffee/src/App.js
+++ khuaycoffee/src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 import ProductDetail from '../src/Components/Product/ProductDetail';
+import Cart from '../src/Components/Cart/Cart';
 
 function App() {
-    var id = "SP01";
+    // var id = "SP01";
+    var idUser = "KH02";
   return (
     <div className="App">
         {/* <a> */}
           {/* <ProductDetail/> */}
-          <ProductDetail id={id} ></ProductDetail>
+          {/* <ProductDetail id={id} ></ProductDetail> */}
+          <Cart idUser = {idUser} />
         {/* </a> */}
 
     </div>
--- khuaycoffee/src/Components/Product/ProductDetail.js
+++ khuaycoffee/src/Components/Product/ProductDetail.js
@@ -6,20 +6,42 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { db, getData, addData, deleteData, updateData, CUSTOMER, ORDER, PRODUCT, CART } from '../../Firebase/firebase';
 import { projectFirestore } from '../../Firebase/firebase';
+import { Cart } from '../Cart/Cart';
 import sp1 from '../../Assets/sp1.jpg';
 
-function ProductDetail({id}) {
+function ProductDetail({ id }) {
 
+    const listGrindDetail = [
+        ["Espresso", "Bình Moka", "Aeropress"],
+        ["Bình Pour-over hình phễu", "Aeropress"],
+        ["Bình Pour-over hình phễu", "Bình pha cà phê đáy phẳng", "Siphon, Aeropress (thời gian pha lâu hơn 3 phút)"],
+        ["French Press", "Bình pha cà phê (Percolator)", "Thử cà phê (Cupping)"]
+    ]
+
+    var productDetail = {};
+    var [priceProduct, setPriceProduct] = useState(0);
+    var [grindSize, showGrindSize] = useState(listGrindDetail[0]);
     const [dataCustomer, setDataCustomer] = useState([]);
     const [dataProduct, setDataProduct] = useState([]);
     const [dataCart, setDataCart] = useState([]);
-    const getDataAll = async() => { 
+    const getDataAll = async () => {
         setDataCustomer(await getData(CUSTOMER, db));
         setDataProduct(await getData(PRODUCT, db));
         setDataCart(await getData(CART, db));
+        for (var i of dataProduct) {
+            if (i.ID === id) {
+                productDetail = i;
+                console.log("find");
+                break;
+            }
+        }
     }
     // console.log(dataCustomer);
-    useEffect(() => getDataAll(), []);
+
+    useEffect(() => {
+        getDataAll();
+        setPriceProduct(productDetail.price);
+    }, []);
 
     console.log(dataCustomer);
     console.log(dataProduct);
@@ -31,23 +53,18 @@ function ProductDetail({id}) {
     // console.log(id);
     // var product = getData(PRODUCT, db);
     // console.log(product);
-    var productDetail = {};
-    for (var i of dataProduct) {
-        if (i.ID === id) {
-            productDetail = i;
-            console.log("find");
-            break;
-        }
-    }
-    console.log(productDetail);
-    const listGrindDetail = [
-        ["Espresso", "Bình Moka", "Aeropress"],
-        ["Bình Pour-over hình phễu", "Aeropress"],
-        ["Bình Pour-over hình phễu", "Bình pha cà phê đáy phẳng", "Siphon, Aeropress (thời gian pha lâu hơn 3 phút)"],
-        ["French Press", "Bình pha cà phê (Percolator)", "Thử cà phê (Cupping)"]
-    ]
 
-    const [grindSize, showGrindSize] = useState(listGrindDetail[0]);
+    // console.log(productDetail);
+    console.log(productDetail.price);
+    // const [priceProduct, setPriceProduct] = useState((Number(productDetail.price)*0.4));
+    // useEffect(() => {
+    //     setPriceProduct(productDetail.price);
+    // }, [])
+
+    console.log(Number(productDetail.price));
+    console.log(priceProduct);
+
+    // const [priceProduct, setPriceProduct] = useState(100000/4);
 
     // const [productDetail, showProductDetail] = useState ([
     //     {id: '1', name: 'Cà phê Ánh Sáng G', price: 100000, component: 'Arabica, Robusta, Excelsa, Catimor', mass: 250, image: 'https://trungnguyencoffeevn.com/wp-content/uploads/2018/09/sang20tao204-218569j9752.jpg' },
@@ -74,56 +91,6 @@ function ProductDetail({id}) {
     //     document.querySelector(".ajaxcart__row.mustDelete").remove();
     // }
     // function AddCart() {
-    //     // console.log("add");
-    //     var cart = document.getElementById("CartDrawer");
-    //     cart.classList.add("active");
-    //     document.querySelector(".background-body").classList.add("active");
-    //     var newCart = document.createElement("div");
-    //     // newCart.innerHTML = `\
-    //     // <div class="ajaxcart__product cart_product" data-line="1">\
-    //     //     <div class="cart_image">\
-    //     //         <img width="80" height="80"\
-    //     //             src="${document.querySelector(".detail-product .img-product").src}"\
-    //     //             alt="${document.querySelector(".detail-product-header h2").innerText}"> </img>\
-    //     //     </div>\
-    //     //     <div class="grid__item cart_info">\
-    //     //         <div class="ajaxcart__product-name-wrapper cart_name">\
-    //     //             <p>${document.querySelector(".detail-product-header h2").innerText}</p>\
-    //     //             <span class="ajaxcart__product-meta variant-title">500.000đ</span>\
-    //     //         </div>\
-    //     //         <div class="grid">\
-    //     //             <div class="grid__item one-half cart_select cart_item_name">\
-    //     //                 <label class="cart_quantity">Số lượng</label>\
-    //     //                 <div class="ajaxcart__qty input-group-btn clearfix">\
-    //     //                     <button type="button"\
-    //     //                     onclick="var result = this.parentNode.childNodes[3]; var qty = result.value; if( qty > 1 ) result.value--;\
-    //     //                         this.parentNode.childNodes[3].value = result.value;"\
-    //     //                         class="ajaxcart__qty-adjust ajaxcart__qty--minus items-count"\
-    //     //                         data-id="" data-qty="0" data-line="1" aria-label="-">\
-    //     //                         -\
-    //     //                     </button>\
-    //     //                     <input type="text" name="updates[]"\
-    //     //                         class="ajaxcart__qty-num number-sidebar" maxlength="3" value=${document.querySelector("#qty").value}\
-    //     //                         min="0" data-id="" data-line="1" aria-label="quantity"\
-    //     //                         pattern="[0-9]*"> </input>\
-    //     //                     <button type="button"\
-    //     //                         onclick=" var result = this.parentNode.childNodes[3]; var qty = result.value; if( !isNaN(qty)) result.value++;"\
-    //     //                         class="ajaxcart__qty-adjust ajaxcart__qty--plus items-count"\
-    //     //                         data-id="" data-line="1" data-qty="2" aria-label="+">\
-    //     //                         +\
-    //     //                     </button>\
-    //     //                 </div>\
-    //     //             </div>\
-    //     //             <div class="grid__item one-half text-right cart_prices">\
-    //     //                 <span class="cart-price">${document.querySelector('.product-price p.text-end').innerText}</span>\
-    //     //                 <a 
-    //     //                     onclick="console.log(this); var = this; while(!a.parentNode.classList.contains('ajaxcart__row')) a = a.parentNode; a.remove();" \
-    //     //                     class="cart__btn-remove remove-item-cart ajaxifyCart--remove"\
-    //     //                     href="javascript:;" data-line="1">Xóa</a>\
-    //     //             </div>\
-    //     //         </div>\
-    //     //     </div>\
-    //     // </div>`;
 
     //     var cartTotal = document.querySelector(".CartSideContainer .ajaxcart__inner");
     //     cartTotal.appendChild(newCart);
@@ -184,7 +151,7 @@ function ProductDetail({id}) {
     //                                                     <input id="qty" type="text" maxLength={3} defaultValue={1} onChange={() => { var num = document.getElementById('qty'); if (num.value === "0") num.value = 1; }} />
     //                                                     <button className="change-qty" onClick={() => { var num = document.getElementById('qty'); var qty = num.value; if (qty < 999) num.value++; }} type="button">
     //                                                         <i className="fa fa-plus-circle" aria-hidden="true"></i></button>
-                                                        
+
     //                                                 </div>
     //                                             </div>
     //                                             <div className="col-8">
@@ -354,21 +321,138 @@ function ProductDetail({id}) {
     // )
     return (
         <>
-            <div className="body">
+            <div className="body-product">
                 <div className="container">
                     <div className="detail-product">
                         <div className="row">
                             <div className="col-5">
-                                <img src={sp1} className="img-product img-fluid" alt="ảnh sản phẩm"> </img>
-                                <h1>ABC product</h1>
-                                
+                                <img src={sp1} className="img-product img-fluid" alt="ảnh sản phẩm" />
+
                             </div>
-                            
+                            <div className="col-7 prefix">
+                                <div className="detail-product-header">
+                                    <h2>{productDetail.name}</h2>
+                                </div>
+                                <div className="detail-product-bot">
+                                    <form id="add-to-cart">
+                                        <div className="row">
+                                            <div className="col-4">
+                                                <p>Kích cỡ xay:</p>
+                                                <div className="input-grind">
+                                                    <input onClick={() => showGrindSize(listGrindDetail[0])} type="radio" id="min" name="grind" defaultValue="Mịn" defaultChecked="checked" />
+                                                    <label htmlFor="min">Mịn</label><br />
+                                                </div>
+                                                <div className="input-grind">
+                                                    <input onClick={() => showGrindSize(listGrindDetail[1])} type="radio" id="hoi-min" name="grind" defaultValue="Hơi mịn" />
+                                                    <label htmlFor="hoi-min">Hơi mịn</label><br />
+                                                </div>
+                                                <div className="input-grind">
+                                                    <input onClick={() => showGrindSize(listGrindDetail[2])} type="radio" id="vua" name="grind" defaultValue="Vừa" />
+                                                    <label htmlFor="vua">Vừa</label>
+                                                </div>
+                                                <div className="input-grind">
+                                                    <input onClick={() => showGrindSize(listGrindDetail[3])} type="radio" id="tho" name="grind" defaultValue="Thô" />
+                                                    <label htmlFor="tho">Thô</label>
+                                                </div>
+                                            </div>
+                                            <div className="col-8">
+                                                <div className="product-element">
+                                                    <p className="bold">Thành phần:</p>
+                                                    <p>{productDetail.makeof}</p>
+                                                </div>
+                                                <div className="caffeine">
+                                                    <p className="bold">Hàm lượng caffeine:</p>
+                                                    <p>{productDetail.content}</p>
+                                                </div>
+                                                <div className="input-weight">
+                                                    <p className="bold">Khối lượng:</p>
+                                                    <div className="input-grind">
+                                                        <input onChange={() => setPriceProduct(Number(priceProduct) / 32)} type="radio" id="250g" name="weight" defaultValue="250g" defaultChecked="checked" />
+                                                        <label htmlFor="250g">250g</label><br />
+                                                    </div>
+                                                    <div className="input-grind">
+                                                        <input onChange={() => setPriceProduct(Number(priceProduct) * 32)} type="radio" id="10kg" name="weight" defaultValue="10kg" />
+                                                        <label htmlFor="10kg">10kg</label><br />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-12">
+                                                    <div className="col-4">
+
+                                                        <div className="input-qty prefix">
+                                                            <p className="bold">Số lượng:</p>
+                                                            <button className="change-qty" onClick={() => { var num = document.getElementById('qty'); console.log(num); var qty = num.value; if (qty > 1) num.value--; }} type="button">
+                                                                <i className="fa fa-minus-circle" aria-hidden="true"></i></button>
+                                                            <input id="qty" type="text" maxLength={4} defaultValue={1} onChange={() => { var num = document.getElementById('qty'); if (num.value === "0") num.value = 1; }} />
+                                                            <button className="change-qty" onClick={() => { var num = document.getElementById('qty'); var qty = num.value; if (qty < 999) num.value++; }} type="button">
+                                                                <i className="fa fa-plus-circle" aria-hidden="true"></i>
+                                                            </button>
+
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-8">
+                                                        <div className="product-price prefix">
+                                                            <p className="text-start bold">Giá:</p>
+                                                            {/* <p className="text-end">{String(priceProduct)} đ</p> */}
+                                                            <p className="text-end">{priceProduct} đ</p>
+                                                        </div>
+                                                        <button type="button" className="btn btn-primary btn-lg btn-gray btn-cart btn_buy add_to_cart">
+                                                            Thêm vào giỏ
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    
+                    <div className="about-product">
+                        <div className="row">
+                            <div className="col-6">
+                                <div className="about-coffee">
+                                    <h2 className="text-center">Mô tả</h2>
+                                    <p>{productDetail.description}</p>
+                                </div>
+                                <hr />
+                                <div className="hint">
+                                    <h2 className="text-center">Gợi ý:</h2>
+                                    <ul>
+                                        {
+                                            grindSize.map((grind, index) => (
+                                                <li key={index}>{grind}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                    <ul className="d-none">
+                                        <li>Espresso</li>
+                                        <li>Bình Moka</li>
+                                        <li>Aeropress</li>
+                                    </ul>
+                                    <ul className="d-none">
+                                        <li>Bình Pour-over hình phễu</li>
+                                        <li>Aeropress</li>
+                                    </ul>
+                                    <ul className="d-none">
+                                        <li>Bình Pour-over hình phễu</li>
+                                        <li>Bình pha cà phê đáy phẳng</li>
+                                        <li>Siphon, Aeropress (thời gian pha lâu hơn 3 phút)</li>
+                                    </ul>
+                                    <ul className="d-none">
+                                        <li>French Press,</li>
+                                        <li>Bình pha cà phê (Percolator)</li>
+                                        <li>Thử cà phê (Cupping)</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-6 about-img">
+                                <img src="https:thumbs.dreamstime.com/b/espresso-coffee-cup-beans-vintage-table-90374872.jpg" className="img-fluid" alt="hình ảnh về cà phê" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                
             </div>
         </>
     )
