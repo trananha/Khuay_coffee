3d1ddc0202e52fab36b6063614bced63165652a6
viethuynh713 <66113968+viethuynh713@users.noreply.github.com>
2022-03-27 22:56:23 +0700

    update

--- khuaycoffee/src/App.js
+++ khuaycoffee/src/App.js
@@ -10,40 +10,33 @@ import Order from './Components/Admin/Order';
 import Product from './Components/Admin/Product';
 import {db,CUSTOMER,getData,updateData,addData,deleteData} from './Firebase/firebase';
 import { account_data } from './Components/Admin/FakeData';
-const addAcount=()=>{
-  for (let i in account_data) {
-    addData(account_data[i],CUSTOMER,db)
-  }
-}
+import Home from './pages/trangchu';
+import SignInInterface from './pages/signin';
+import SignUpInterface from './pages/signup';
 var idSP = "SP01"
 function App() {
   return (
     <>
     
     <div className="App">
-        <a>
-          {/* <AddProduct></AddProduct> */}
-          {/* <OrderDetail id = 'ORD01'></OrderDetail> */}
-          <ChangeProductInfo id ={idSP}></ChangeProductInfo>
-        </a>
         {/* <button>
           <Link to='/admin/accounts' >Admin</Link>
         </button>
         <button onClick={async ()=>{console.log(await getData(CUSTOMER,db))}}>Get</button>
         <button onClick={()=>{console.log(updateData({"testvalue":"hyhyhyhy"},"testid",CUSTOMER,db))}}>Update</button>
         <button onClick={()=>{addAcount()}}>Add</button>
-        <button onClick={()=>{deleteData("Ubblr6mf2umKViVTatni",CUSTOMER,db)}}>delete</button>
+        <button onClick={()=>{deleteData("Ubblr6mf2umKViVTatni",CUSTOMER,db)}}>delete</button> */}
       <Routes>
-        <Route path="/" element={<Banner />} />
+        <Route path="/" element={<Home />} />
         <Route path="/admin/account" element={<Account />} />
         <Route path="/admin/product" element={<Product />} />
         <Route path="/admin/order" element={<Order/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<SignInInterface />} />
+        <Route path="/register" element={<SignUpInterface />} />
         <Route path="/product" element={<Category />} />
         <Router path="/product/:id" element={<ProductDetail />} />
 
-      </Routes> */}
+      </Routes>
     </div>
 
     </>
