235ad78b2b8fa5efac85e6c2b5d6dcb30f3c02a3
krislion01 <quyenmanhkhong@gmail.com>
2022-03-29 23:25:18 +0700

    ====Khoi====Chỉnh ProductCard

--- khuaycoffee/src/Components/Products/ProductCard.js
+++ khuaycoffee/src/Components/Products/ProductCard.js
@@ -1,14 +1,16 @@
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ProductCard (product)
 {
-    console.log("producy",product.props)
+    const navigate=useNavigate();
+    const handleOnClickAdmin = useCallback(() => navigate('/product/'+product.props.docId, {replace: true}), [navigate]);
     return (
         <div className='product'>
           <div className='container'>
-            <img src="https://i.ibb.co/rMLQdcg/1cd3270b54457b-img-2478.png" alt='product'></img>
+            <img src="https://i.ibb.co/rMLQdcg/1cd3270b54457b-img-2478.png" alt='product' onClick={handleOnClickAdmin} style={{cursor:"pointer"}}></img>
           </div>
           <h6 className='cardname'>{product.props.name}</h6>
           <h6 className='cardprice'>{product.props.price+" VND"}</h6>
