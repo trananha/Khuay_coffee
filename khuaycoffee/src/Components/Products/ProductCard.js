318ec04e44047875c85d9674bcbca32a505611e1
viethuynh713 <66113968+viethuynh713@users.noreply.github.com>
2022-03-29 21:25:31 +0700

    Update ProductCard.js

--- khuaycoffee/src/Components/Products/ProductCard.js
+++ khuaycoffee/src/Components/Products/ProductCard.js
@@ -8,7 +8,7 @@ function ProductCard (product)
     return (
         <div className='product'>
           <div className='container'>
-            <img src={""} alt='product'></img>
+            <img src="https://i.ibb.co/rMLQdcg/1cd3270b54457b-img-2478.png" alt='product'></img>
           </div>
           <h6 className='cardname'>{product.props.name}</h6>
           <h6 className='cardprice'>{product.props.price+" VND"}</h6>
